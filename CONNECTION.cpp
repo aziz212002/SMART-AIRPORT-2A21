19fa7cdaf14e8f2e78a2619f0e5d0069248024e6
aziz <benromdhane.mohamedaziz@esprit.tn>
2022-10-31 23:07:17 +0100

    first

---
+++ CONNECTION.cpp
@@ -0,0 +1,23 @@
+#include "CONECTION.h"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("source_projet2A");
+db.setUserName("mohamedaziz");//inserer nom de l'utilisateur
+db.setPassword("aziz12");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+
+
+
+    return  test;
+}
